53e5db03f1b2ff794e81c440c95183b8370f550c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-02-17 21:30:28 -0600

    Update Account with getProfile

--- src/routes/Account.svelte
+++ src/routes/Account.svelte
@@ -1,3 +1,43 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import type { AuthSession } from '@supabase/supabase-js';
+	import { supabase } from '$lib/supabaseClient';
+
+	export let session: AuthSession;
+
+	let loading = false;
+	let username: string | null = null;
+	let website: string | null = null;
+	let avatarUrl: string | null = null;
+
+	onMount(() => {
+		getProfile();
+	});
+
+	const getProfile = async () => {
+		try {
+			loading = true;
+			const { user } = session;
+
+			const { data, error, status } = await supabase
+				.from('profiles')
+				.select(`username, website, avatar_url`)
+				.eq('id', user.id)
+				.single();
+
+			if (data) {
+				username = data.username;
+				website = data.website;
+				avatarUrl = data.avatar_url;
+			}
+
+			if (error && status != 406) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	};
 </script>
