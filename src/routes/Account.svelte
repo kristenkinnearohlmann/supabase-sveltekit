86234cee00a4e68ff1f4997ca9925509d6a16bf3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-02-19 21:23:06 -0600

    Add signOut to Account

--- src/routes/Account.svelte
+++ src/routes/Account.svelte
@@ -65,4 +65,18 @@
 			loading = false;
 		}
 	}
+
+	async function signOut() {
+		try {
+			loading = true;
+			let { error } = await supabase.auth.signOut();
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	}
 </script>
