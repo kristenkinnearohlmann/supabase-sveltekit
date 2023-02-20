28d0420377ff6f848256f0e6f3836687ad1e6b93
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-02-20 16:03:04 -0600

    Add imports to base route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+	import Account from './Account.svelte';
+	import Auth from './Auth.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
