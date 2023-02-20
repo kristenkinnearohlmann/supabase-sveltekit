46b0382822f458e21399bad58cd93dc87d1be8f3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-02-19 21:28:33 -0600

    Add form to Account

--- src/routes/Account.svelte
+++ src/routes/Account.svelte
@@ -80,3 +80,31 @@
 		}
 	}
 </script>
+
+<form class="form-widget" on:submit|preventDefault={updateProfile}>
+	<div>
+		<label for="email">Email</label>
+		<input id="email" type="text" value={session.user.email} disabled />
+	</div>
+	<div>
+		<label for="username">Name</label>
+		<input id="username" type="text" bind:value={username} />
+	</div>
+	<div>
+		<label for="website">Website</label>
+		<input id="website" type="website" bind:value={website} />
+	</div>
+
+	<div>
+		<input
+			type="submit"
+			class="button block primary"
+			value={loading ? 'Loading...' : 'Update'}
+			disabled={loading}
+		/>
+	</div>
+
+	<div>
+		<button class="button block" on:click={signOut} disabled={loading}>Sign Out</button>
+	</div>
+</form>
