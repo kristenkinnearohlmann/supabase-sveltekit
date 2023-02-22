c3ea50f991a439caec01c62fa478e0df16367aee
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-02-22 17:23:26 -0600

    Draft Avatar component

---
+++ src/routes/Avatar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { supabase } from '$lib/supabaseClient';
+
+	export let size = 10;
+	export let url: string;
+
+	let avatarUrl: string | null = null;
+	let uploading = false;
+	let files: FileList;
+
+	const dispatch = createEventDispatcher();
+</script>
